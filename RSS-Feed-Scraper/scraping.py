0e71b7ed1886ce2cd5ad8c5853f859740591d304
Tariq <118364536@umail.ucc.ie>
2021-05-24 13:59:34 +0100

    create function hackernews_rss for scrpaing

--- RSS-Feed-Scraper/scraping.py
+++ RSS-Feed-Scraper/scraping.py
@@ -1,1 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def hackernews_rss('https://news.ycombinator.com/rss'):
+	try:
+		r = requests.get()
+		return print('The scraping job suceeded: ', r.status_code)
+	except Exceptio as e:
+		print('The scraping job failed. See exception: ')
+		print(e)
+
+print('Starting Scrpaing')
+hackernews_rss()
+print('Finished Scraping')
+
 
